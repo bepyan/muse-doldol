e1cb9b495007416e6d284635e6caa6d21d5e9a78
edward <bepyan@naver.com>
2023-03-13 18:25:49 +0900

    🚧 test router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import { useState } from 'preact/hooks';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom';
 
 export default function App() {
+  return (
+    <BrowserRouter basename="muse-doldol">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path=":slug" element={<Slug />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Navbar() {
   const [count, setCount] = useState(0);
+  const navigate = useNavigate();
 
   return (
     <div>
       <h2>돌돌말이 김밥</h2>
       <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      <BrowserRouter></BrowserRouter>
+      <div class="flex items-center gap-2">
+        <button onClick={() => navigate('/')}>home</button>
+        <button onClick={() => navigate('/test')}>test</button>
+      </div>
     </div>
   );
+}
+
+function Home() {
+  return <div>home</div>;
+}
+
+function Slug() {
+  const { slug } = useParams();
+  return <div>{slug}</div>;
 }
